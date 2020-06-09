3172f89328fd855ba59751828f6ab3701beea0f7
ushliypakostnik <levon.gambaryan@gmail.com>
2020-06-10 02:23:03 +0300

    start docs

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  title: 'Как верстать веб-интерфейсы быстро, качественно и интересно.',
+  description: 'Как верстать веб-интерфейсы быстро, качественно и интересно',
+  base: '/',
+  sidebarDepth: 5,
+  themeConfig: {
+    nav: [
+      {
+        text: 'VK',
+        link: 'https://vk.com/levon_gambaryan',
+      },
+      {
+        text: 'FB',
+        link: 'https://www.facebook.com/gambaryan',
+      }
+    ],
+    sidebar: [
+      {
+        title: `Азы`,
+        collapsable: false,
+        // path: '/start/',
+        children: [
+          {
+            title: 'Препроцессоры',
+            path: '/start/bad',
+          },
+          {
+            title: 'Абстракция',
+            path: '/start/abstract',
+          },
+        ]
+      },
+    ],
+  },
+};
