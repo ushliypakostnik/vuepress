4cf83ef7c223a2f117f02e616ce201da55f3fcca
ushliypakostnik <levon.gambaryan@gmail.com>
2020-06-14 01:57:25 +0300

    start second part

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -12,7 +12,15 @@ module.exports = {
       {
         text: 'FB',
         link: 'https://www.facebook.com/gambaryan',
-      }
+      },
+      {
+        text: 'HH',
+        link: 'https://spb.hh.ru/resume/7c1eac71ff010d7c4d0039ed1f6843664c5172',
+      },
+      {
+        text: 'ХабрКарьера',
+        link: 'https://career.habr.com/levon-gambaryan',
+      },
     ],
     sidebar: [
       {
