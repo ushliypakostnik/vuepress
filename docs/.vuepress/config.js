d900922d1f9d49279d08384b1a3123622c3574de
ushliypakostnik <levon.gambaryan@gmail.com>
2020-06-21 17:40:56 +0300

    orfo fixes

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -14,12 +14,24 @@ module.exports = {
         link: 'https://www.facebook.com/gambaryan',
       },
       {
+        text: 'Instagram',
+        link: 'https://www.instagram.com/levongambaryan/',
+      },
+      {
+        text: 'GitHub',
+        link: 'https://github.com/ushliypakostnik',
+      },
+      {
         text: 'HH',
         link: 'https://spb.hh.ru/resume/7c1eac71ff010d7c4d0039ed1f6843664c5172',
       },
       {
         text: 'ХабрКарьера',
         link: 'https://career.habr.com/levon-gambaryan',
+      },
+      {
+        text: 'Linkedin',
+        link: 'https://www.linkedin.com/in/%D0%BB%D0%B5%D0%B2%D0%BE%D0%BD-%D0%B3%D0%B0%D0%BC%D0%B1%D0%B0%D1%80%D1%8F%D0%BD-47606b192/',
       },
     ],
     sidebar: [
